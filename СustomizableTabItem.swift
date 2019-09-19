cb997c04ec28b46db536ab399eb3ba650d97cd9d
Danil Kristalev <kristalev.danil@gmail.com>
2019-09-19 17:58:14 +0700

    Add customizable tabs

---
+++ СustomizableTabItem.swift
@@ -0,0 +1,13 @@
+//
+//  СustomizableTabItem.swift
+//  KPCTabsControl
+//
+//  Created by Danil Kristalev on 19/09/2019.
+//  Copyright © 2019 Cédric Foellmi. All rights reserved.
+//
+
+import Cocoa
+
+public protocol СustomizableTabItem {
+  var tabStyle: Style? { get }
+}
